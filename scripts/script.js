7811c2c72a7e4935fda65773c2481ab96efc4895
Molly <mjb@git.ca>
2024-11-29 14:03:46 -0800

    put sidebar code in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -69,3 +69,17 @@ function fetchWeatherData(city) {
       weatherResult.style.display = "none";
     });
 }
+
+//sidebar
+
+document.getElementById('toggle-button').addEventListener('click', function() {
+  var sidebar = document.getElementById('sidebar');
+  var button = document.getElementById('toggle-button');
+  if (sidebar.classList.contains('open')) {
+      sidebar.classList.remove('open');
+      button.classList.remove('open');
+  } else {
+      sidebar.classList.add('open');
+      button.classList.add('open');
+  }
+});
